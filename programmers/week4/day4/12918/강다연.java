7e2b981c0d461a696fe3ea91f6866642491d692c
K-Dayeon03 <zns030@gmail.com>
2026-06-11 17:38:50 +0900

    Add: dayeon-12918

--- programmers/week4/day4/12918/강다연.java
+++ programmers/week4/day4/12918/강다연.java
@@ -1,8 +1,10 @@
 class Solution {
+
     public boolean solution(String s) {
         if (s.length() != 4 && s.length() != 6) {
             return false;
         }
+        //예외처리
         try {
             Integer.parseInt(s);
             return true;

a4751c41c7c07f5ac5924b603d39cc385d13fbb6
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-07-28 20:18:39 +0900

    Add: junseo-42576

---
+++ programmers/week10/day1/42576/장준서.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+  public String solution(String[] participant, String[] completion) {
+
+
+    HashMap<String, Integer> map = new HashMap<>();// 중복 체크도 같이 함 그래서 헤시맵
+
+
+    for (String p : participant) {
+      map.put(p, map.getOrDefault(p, 0) + 1);
+    }
+
+
+    for (String c : completion) {
+      map.put(c, map.get(c) - 1);
+    }
+
+
+    for (String key : map.keySet()) {
+      if (map.get(key) != 0) {
+        return key;
+      }
+    }
+
+    return null;
+  }
+}

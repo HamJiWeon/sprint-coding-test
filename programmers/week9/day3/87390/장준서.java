a0d3e5b912ebe91c53c63b43e5b3e8e213e62d90
애니프사희망 <jangjunseo518@gmail.com>
2026-07-28 22:43:25 +0900

    Add: junseo - 87390 (#207)
    Co-authored-by: jangjunseo518-collab <wkdwnstj00@naver.com>

---
+++ programmers/week9/day3/87390/장준서.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int[] solution(int n, long left, long right) {
+
+
+    int[] rl = new int[(int) (right - left +1)];
+
+    for(long i = left; i <= right; i++ ){
+
+      long row = i / n;
+      long col = i % n;
+      rl[(int) (i - left)] = (int) (Math.max(row, col) + 1);
+
+
+    }
+
+    return rl;
+  }
+}

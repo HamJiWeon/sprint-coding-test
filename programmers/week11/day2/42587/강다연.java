c5704e9c941152e2ca0e54afc04ffd7a83997bd6
강다연 <142641618+K-Dayeon03@users.noreply.github.com>
2026-08-11 22:37:31 +0900

    [11주차] DAY2-dayeon (#229)
    * Add : 문자열을 정수로 바꾸기-12925
    
    * Add : 약수의 합 구하기
    
    * Refactor : IntStream add
    
    Refactor solution method to use IntStream for summing divisors.
    
    * Rename : dayeon-12928
    
    * Add : dayeon - 12931
    
    * Add : dayeon - 12932
    
    * Add : dayeon - 12933
    
    * Add: week7_오답노트
    
    * Add: Week6,Week8
    
    * Add: dayeon-42586
    
    * Add:dayeon-42587

---
+++ programmers/week11/day2/42587/강다연.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            boolean rank0 = false;
+
+            for (int index : queue) {
+                //인덱스 위치에 있는 값이, 현재 값보다 높으면 true
+                if (priorities[index] > priorities[now]) {
+                    rank0 = true;
+                    break;
+                }
+            }
+
+            if (rank0) {
+                queue.add(now);
+            } else {
+                answer++;
+
+                if (now == location) {
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
